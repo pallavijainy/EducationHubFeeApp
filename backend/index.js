83f9c26733f5db07b31f3f9676065c1f47e52178
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-07-15 12:41:02 +0530

    add

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,34 @@
 const express = require("express");
 const { Connection } = require("./config/db");
+const { StudentModel } = require("./model/StudentModel");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   res.send("pallavi");
+});
+
+//add student
+app.post("/newstudentadd", async (req, res) => {
+  const newstudent = req.body;
+  try {
+    const data = new StudentModel(newstudent);
+    await data.save();
+    res.send("New student Added");
+  } catch (error) {
+    res.send({ msg: "something went wrong" });
+  }
+});
+
+//make payment
+app.post("/payment/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const studentdata = StudentModel.findOne({ _id: id });
+  } catch (error) {
+    res.send({ msg: "something went wrong" });
+  }
 });
 
 app.listen(8080, async () => {
--- backend/model/PaymentModel.js
+++ backend/model/PaymentModel.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 
-const PaymentSchema = mongoose.Schema({}, { timestamps: true });
+const PaymentSchema = mongoose.Schema(
+  {
+    student: { type: mongoose.Schema.type.ObjectId, ref: "Student" },
+    feepaid: String,
+  },
+  { timestamps: true }
+);
 
 const PaymentModel = mongoose.model("payment", PaymentSchema);
 
